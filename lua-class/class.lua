c03f8cce8ce342e7fee2286ac3c84b1ae3c43088
wangzhen <852511438@qq.com>
2018-08-28 18:53:18 +0800

    优化class脚本, 修复调用重写方法逻辑的bug,增加部分注释

--- lua-class/class.lua
+++ lua-class/class.lua
@@ -9,13 +9,14 @@ function class(base)
     
     local vtbl = {}
     _class[class_type] = vtbl
-    setmetatable(class_type,{__newindex = vtbl, __index = vtbl})
+    setmetatable(class_type, {__newindex = vtbl, __index = vtbl})
  
     if base then
-        setmetatable(vtbl,{__index=
+        setmetatable(vtbl, {__index=
             function(t,k)
-                local ret=_class[base][k]
-                vtbl[k]=ret
+                local ret = _class[base][k]
+                --缓存调用函数到当前类中, 如果子类中没有的父类函数, 缓存后会加快调用速度
+                vtbl[k] = ret
                 return ret
             end
         })
@@ -48,33 +49,27 @@ function class(base)
         assert(self and self.__type == 'object', string.format("'self' must be a object when call super(self, '%s', ...)", tostring(f)))
  
         local originBase = self.__base
-        --find the first f function that differ from self[f] in the inheritance chain
-        local s     = originBase
-        local base  = s.__base
-        while base and s[f] == base[f] do
-            s = base
+        local base  = originBase.__base
+        --从_class类注册表中使用基类获取对应函数, 不触发__index方式, 在继承链中循环查找
+        while base and rawget(_class[base], f) == nil do
             base = base.__base
         end
         
-        assert(base and base[f], string.format("base class or function cannot be found when call .super(self, '%s', ...)", tostring(f)))
-        --now base[f] is differ from self[f], but f in base also maybe inherited from base's baseClass
-        while base.__base and base[f] == base.__base[f] do
+        --断言是否从继承链中找到包含 f 函数的基类
+        assert(base, string.format("base class %s function cannot be found when call .super(self, '%s', ...)", tostring(f), tostring(f)))
+        --找到base[f]方法后, 可能在经过一次调用后, 由于上面有缓存函数的逻辑, 对应找到的基类可能并不是实际的包含 f 函数的基类, 所以需要继续寻找真正包含f 函数的基类
+        while base.__base and base[f] == base.__base[f] do 
             base = base.__base
         end
  
-        -- If the base also has a baseclass, temporarily set :super to call that baseClass' methods
-        -- this is to avoid stack overflow
-        if base.__base then
-            self.__base = base
-        end
+        --临时指定该父类为当前调用对象的基类, 避免调用函数内存在还继续调用父类的逻辑, 导致循环调用堆栈溢出
+        self.__base = base
  
-        --now, call the super function
+        --开始调用函数
         local result = base[f](self, ...)
  
-        --set back
-        if base.__base then
-            self.__base = originBase
-        end
+        --临时指定基类为包含函数的基类, 调用完方法后再指定回原基类
+        self.__base = originBase
  
         return result
     end
