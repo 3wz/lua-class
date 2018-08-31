e0aa11898035d49fad9d4619a0e9b7036d30c82e
wangzhen <852511438@qq.com>
2018-08-31 18:35:18 +0800

    修复当调用父类方法后, 再调用回子类方法, 然后再通过子类调用父类方法会导致错误

--- lua-class/class.lua
+++ lua-class/class.lua
@@ -44,34 +44,41 @@ function class(base)
         end
         return obj
     end
- 
-    class_type.super = function(self, f, ...)
+
+    class_type.super = function(self, t, f, ...)
         assert(self and self.__type == 'object', string.format("'self' must be a object when call super(self, '%s', ...)", tostring(f)))
  
-        local originBase = self.__base
-        local base  = originBase.__base
-        --从_class类注册表中使用基类获取对应函数, 不触发__index方式, 在继承链中循环查找
-        while base and rawget(_class[base], f) == nil do
-            base = base.__base
-        end
-        
-        --断言是否从继承链中找到包含 f 函数的基类
-        assert(base, string.format("base class %s function cannot be found when call .super(self, '%s', ...)", tostring(f), tostring(f)))
-        --找到base[f]方法后, 可能在经过一次调用后, 由于上面有缓存函数的逻辑, 对应找到的基类可能并不是实际的包含 f 函数的基类, 所以需要继续寻找真正包含f 函数的基类
-        while base.__base and base[f] == base.__base[f] do 
-            base = base.__base
-        end
- 
-        --临时指定该父类为当前调用对象的基类, 避免调用函数内存在还继续调用父类的逻辑, 导致循环调用堆栈溢出
-        self.__base = base
- 
-        --开始调用函数
+        local base = t.__base
+        assert(base, "尝试调用不存在的父类方法!!!!")
         local result = base[f](self, ...)
- 
-        --临时指定基类为包含函数的基类, 调用完方法后再指定回原基类
-        self.__base = originBase
- 
         return result
+
+        --[[旧版方式实现调用父类方式, 该方式会在这种情况下出错: 调用父类方法后, 再调用回子类方法, 然后再通过子类调用父类方法会导致错误
+            local originBase = self.__base
+            local base  = originBase.__base
+            --从_class类注册表中使用基类获取对应函数, 不触发__index方式, 在继承链中循环查找
+            while base and rawget(_class[base], f) == nil do
+                base = base.__base
+            end
+            
+            --断言必须能从继承链中找到包含 f 函数的基类, 否则报错提示尝试调用不存在的父类方法
+            assert(base, string.format("base class %s function cannot be found when call .super(self, '%s', ...)", tostring(f), tostring(f)))
+            --找到base[f]方法后, 可能在经过一次调用后, 由于上面有缓存函数的逻辑, 对应找到的基类可能并不是实际的包含 f 函数的基类, 所以需要继续寻找真正包含f 函数的基类
+            while base.__base and base[f] == base.__base[f] do 
+                base = base.__base
+            end
+    
+            --临时指定该父类为当前调用对象的基类, 避免调用函数内存在还继续调用父类的逻辑, 导致循环调用堆栈溢出
+            self.__base = base
+    
+            --开始调用函数
+            local result = base[f](self, ...)
+    
+            --临时指定基类为包含函数的基类, 调用完方法后再指定回原基类
+            self.__base = originBase
+    
+            return result
+        ]]
     end
  
     return class_type
